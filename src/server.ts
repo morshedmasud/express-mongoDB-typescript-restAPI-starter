9ef4a7b5e64ab96fb7af3557c6ca33e8ad8432fb
morshedmasud <masudraj6@gmail.com>
2022-03-03 18:31:03 +0600

    feat: mongooes senitizer added

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ require("dotenv/config");
 
 import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
+import mongoSanitize from "express-mongo-sanitize";
 import expressRateLimit from "../config/express-rate";
+import expressSlowDown from "../config/express-slow-down";
 import logger from "../config/logger";
 import dbConnect from "../config/mongoose";
 
@@ -15,6 +17,7 @@ const router = express.Router();
 app.use(morgan); // Http logger
 app.use(bodyParser.json()); // Json parse
 app.use(cors); // Cors set
+app.use(mongoSanitize); // sanitize mongoose data
 
 router.get("/", [], async (req: Request, res: Response) => {
   return res.status(200).send("tst");
@@ -25,6 +28,7 @@ app.use(router);
 
 if (process.env.NODE_ENVIRONMENT === "production") {
   app.use(expressRateLimit); // per window rate limit
+  app.use(expressSlowDown); // slow down rather then block
 }
 
 const port = process.env.PORT || 3000;

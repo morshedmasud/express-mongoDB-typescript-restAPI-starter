22f2aeff39a37859a8a908cd719a03c6ccf8e151
morshedmasud <masudraj6@gmail.com>
2022-04-05 04:19:10 +0600

    feat: Basic Authentication with passport-http stretegy

--- config/passport-http.ts
+++ config/passport-http.ts
@@ -1,17 +1,5 @@
 import passportHttp from "passport-http";
 
-// const passportHttpInit = new passportHttp.BasicStrategy(
-//   catchAsyncErr(async (name: string, secret: string, done: any) => {
-//     const client =
-//       name === process.env.JWT_BASIC_USER &&
-//       secret === process.env.JWT_BASIC_SECRET
-//         ? { name, secret }
-//         : null;
-//     if (!client) return done("invalid client", client);
-//     return done(null, client);
-//   })
-// );
-
 const passportHttpInit = new passportHttp.BasicStrategy(
   async (name: string, secret: string, done) => {
     const client =
